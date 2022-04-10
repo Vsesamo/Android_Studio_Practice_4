2117f17007613920f4dcbf9b5546b8f5877db3fb
Alekset Shteyn <Vsesamo@mail.ru>
2022-04-10 18:30:52 +0300

    Initial commit

---
+++ looper/src/main/java/ru/mirea/shteyn/looper/MainActivity.java
@@ -0,0 +1,30 @@
+package ru.mirea.shteyn.looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyLooper myLooper = new MyLooper();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myLooper.start();
+    }
+
+    public void onClick(View view) {
+        Message msg = new Message();
+        Bundle bundle = new Bundle();
+        bundle.putString("KEY", "Будущий программист");
+        msg.setData(bundle);
+        if (myLooper != null) {
+            myLooper.handler.sendMessage(msg);
+        }
+    }
+}
